10891c5a91a5c920e7cf8b30c65e0be7d7fab386
c4710n <c4710n@users.noreply.github.com>
2022-11-22 20:40:56 +0800

    mix new cozy_aliyun_oss --module CozyAliyunOSS

---
+++ lib/cozy_aliyun_oss.ex
@@ -0,0 +1,18 @@
+defmodule CozyAliyunOSS do
+  @moduledoc """
+  Documentation for `CozyAliyunOSS`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CozyAliyunOSS.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cozy_aliyun_oss_test.exs
@@ -0,0 +1,8 @@
+defmodule CozyAliyunOSSTest do
+  use ExUnit.Case
+  doctest CozyAliyunOSS
+
+  test "greets the world" do
+    assert CozyAliyunOSS.hello() == :world
+  end
+end
