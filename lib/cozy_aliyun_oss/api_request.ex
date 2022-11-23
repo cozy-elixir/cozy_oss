6d576bacbe62cb82829562660da3d66f56f08302
c4710n <c4710n@users.noreply.github.com>
2022-11-23 15:48:00 +0800

    feat: add `CozyAliyunOSS.ApiRequest`

---
+++ lib/cozy_aliyun_oss/api_request.ex
@@ -0,0 +1,251 @@
+defmodule CozyAliyunOSS.ApiRequest do
+  @moduledoc """
+  Converts `%ApiSpec{}` to a `%ApiRequest{}`.
+  """
+
+  defstruct [
+    :scheme,
+    :host,
+    :port,
+    :method,
+    :path,
+    :query,
+    :headers,
+    :body,
+    private: %{}
+  ]
+
+  @typedoc """
+  Request scheme.
+  """
+  @type scheme() :: :https
+
+  @typedoc """
+  Request method.
+  """
+  @type method() :: String.t()
+
+  @typedoc """
+  Request path.
+  """
+  @type path() :: String.t()
+
+  @typedoc """
+  Optional request query.
+  """
+  @type query() :: %{
+          optional(query_name :: String.t()) => query_value :: boolean() | number() | String.t()
+        }
+
+  @typedoc """
+  Request headers.
+  """
+  @type headers() :: %{optional(header_name :: String.t()) => header_value :: String.t()}
+
+  @typedoc """
+  Optional request body.
+  """
+  @type body() :: iodata() | nil
+
+  @type private_metadata() :: %{optional(atom()) => term()}
+
+  @type t :: %__MODULE__{
+          scheme: scheme(),
+          host: String.t() | nil,
+          port: :inet.port_number(),
+          method: method(),
+          path: String.t(),
+          query: query(),
+          headers: headers(),
+          body: body(),
+          private: %{}
+        }
+
+  alias CozyAliyunOSS.Config
+  alias CozyAliyunOSS.ApiSpec
+
+  @spec build!(Config.t(), ApiSpec.t(), keyword()) :: __MODULE__.t()
+  def build!(%Config{} = config, %ApiSpec{} = api_spec, opts) do
+    build_request(config, api_spec)
+    |> set_essential_headers()
+    |> then(&set_signature(config, &1, opts))
+  end
+
+  defp build_request(config, api_spec) do
+    base_url = "https://#{config.host}"
+
+    %{
+      scheme: scheme,
+      host: host,
+      port: port
+    } = parse_base_url(base_url)
+
+    %{
+      bucket: bucket,
+      object: object,
+      sub_resources: sub_resources,
+      method: method,
+      path: path,
+      query: query,
+      headers: headers,
+      body: body
+    } = api_spec
+
+    %__MODULE__{
+      scheme: scheme,
+      host: host,
+      port: port,
+      method: method,
+      path: path,
+      query: query,
+      headers: headers,
+      body: body,
+      private: %{
+        bucket: bucket,
+        object: object,
+        sub_resources: sub_resources
+      }
+    }
+  end
+
+  defp parse_base_url(url) when is_binary(url) do
+    url
+    |> URI.parse()
+    |> Map.take([:scheme, :host, :port])
+  end
+
+  defp set_essential_headers(%__MODULE__{} = req) do
+    req
+    |> set_header_lazy("content-md5", fn -> md5_hash(req.body) end)
+    |> set_header_lazy("content-type", fn -> get_content_type(req) end)
+    |> set_header_lazy("date", fn -> gmt_now() end)
+  end
+
+  defp get_content_type(_req) do
+    "application/octet-stream"
+  end
+
+  defp set_signature(%Config{} = config, %__MODULE__{} = req, opts) do
+    sign_on = Keyword.get(opts, :sign_on, :header)
+
+    case sign_on do
+      :header -> set_signature_on_header(config, req)
+      :query -> set_signature_on_query(req)
+      _ -> raise ArgumentError, "unknown :sign_on value - #{inspect(sign_on)}"
+    end
+  end
+
+  defp set_signature_on_header(%Config{} = config, %__MODULE__{} = req) do
+    signature =
+      req
+      |> build_string_to_sign_for_header_signature()
+      |> hmac_sha1(config.access_key_secret)
+
+    set_header(req, "authorization", "OSS #{config.access_key_id}:#{signature}")
+  end
+
+  defp set_signature_on_query(%__MODULE__{} = req) do
+  end
+
+  @doc false
+  def build_string_to_sign_for_header_signature(%__MODULE__{} = req) do
+    %{
+      headers: headers,
+      private: %{bucket: bucket, object: object, sub_resources: sub_resources}
+    } = req
+
+    [
+      req.method,
+      fetch_header!(req, "content-md5"),
+      fetch_header!(req, "content-type"),
+      fetch_header!(req, "date"),
+      canonicalize_oss_headers(headers),
+      canonicalize_resource(bucket, object, sub_resources)
+    ]
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.join("\n")
+  end
+
+  # creation of CanonicalizedOSSHeaders
+  # https://www.alibabacloud.com/help/en/object-storage-service/latest/access-control-include-signatures-in-the-authorization-header#section-w2k-sw2-xdb
+  @doc false
+  def canonicalize_oss_headers(headers) when is_map(headers) do
+    headers
+    |> Enum.filter(fn {k, _v} -> String.match?(k, ~r/^x-oss-/i) end)
+    |> Enum.sort_by(fn {k, _v} -> k end, :asc)
+    |> Enum.map(fn
+      {k, nil} -> to_string(k)
+      {k, v} -> "#{k}:#{v}"
+    end)
+    |> Enum.join("\n")
+  end
+
+  # creation of CanonicalizedResource
+  # https://www.alibabacloud.com/help/en/object-storage-service/latest/access-control-include-signatures-in-the-authorization-header#section-rvv-dx2-xdb
+  @doc false
+  def canonicalize_resource(bucket, object, sub_resources) do
+    encoded_resource =
+      cond do
+        bucket && object -> Path.join(["/", bucket, object])
+        bucket -> "/#{bucket}/"
+        true -> "/"
+      end
+
+    encoded_sub_resource = encode_sub_resources(sub_resources)
+
+    [
+      encoded_resource,
+      encoded_sub_resource
+    ]
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.join("?")
+  end
+
+  defp encode_sub_resources(sub_resources) do
+    sub_resources
+    |> Enum.sort_by(fn {k, _v} -> k end, :asc)
+    |> Enum.map(fn
+      {k, nil} -> to_string(k)
+      {k, v} -> "#{k}=#{v}"
+    end)
+    |> Enum.join("&")
+  end
+
+  defp set_header(%__MODULE__{} = req, name, value)
+       when is_binary(name) and is_binary(value) do
+    name = String.downcase(name)
+    new_headers = Map.put(req.headers, name, value)
+    %{req | headers: new_headers}
+  end
+
+  defp set_header_lazy(%__MODULE__{} = req, name, fun)
+       when is_binary(name) and is_function(fun, 0) do
+    name = String.downcase(name)
+    new_headers = Map.put_new_lazy(req.headers, name, fun)
+    %{req | headers: new_headers}
+  end
+
+  defp fetch_header!(%__MODULE__{} = req, name) do
+    Map.fetch!(req.headers, name)
+  end
+
+  @doc false
+  def md5_hash(nil), do: md5_hash("")
+
+  def md5_hash(data) do
+    data
+    |> then(&:crypto.hash(:md5, &1))
+    |> Base.encode64()
+  end
+
+  @doc false
+  def hmac_sha1(data, secret) do
+    :crypto.mac(:hmac, :sha, secret, data)
+    |> Base.encode64()
+  end
+
+  defp gmt_now() do
+    now = DateTime.utc_now()
+    Calendar.strftime(now, "%a, %d %b %Y %H:%M:%S GMT")
+  end
+end
---
+++ test/cozy_aliyun_oss/api_request_test.exs
@@ -0,0 +1,72 @@
+defmodule CozyAliyunOSS.ApiRequestTest do
+  use ExUnit.Case
+  alias CozyAliyunOSS.ApiRequest
+
+  describe "important underlying functions works as expected, such as" do
+    test "build_string_to_sign_for_header_signature/1" do
+      request = %ApiRequest{
+        scheme: :https,
+        host: "examplebucket.oss-cn-hangzhou.aliyuncs.com",
+        port: 443,
+        method: "PUT",
+        path: "/nelson",
+        query: %{},
+        headers: %{
+          "content-md5" => "eB5eJF1ptWaXm4bijSPyxw==",
+          "content-type" => "text/html",
+          "date" => "Thu, 17 Nov 2005 18:49:58 GMT",
+          "x-oss-meta-author" => "foo@example.com",
+          "x-oss-meta-magic" => "abracadabra"
+        },
+        body: nil,
+        private: %{
+          bucket: "examplebucket",
+          object: "nelson",
+          sub_resources: %{}
+        }
+      }
+
+      assert ApiRequest.build_string_to_sign_for_header_signature(request) ==
+               "PUT\neB5eJF1ptWaXm4bijSPyxw==\ntext/html\nThu, 17 Nov 2005 18:49:58 GMT\nx-oss-meta-magic:abracadabra\nx-oss-meta-author:foo@example.com\n/examplebucket/nelson"
+    end
+
+    test "canonicalize_oss_headers/1" do
+      assert ApiRequest.canonicalize_oss_headers(%{}) == ""
+
+      assert ApiRequest.canonicalize_oss_headers(%{
+               "x-oss-meta-a" => "a",
+               "x-oss-meta-c" => "c",
+               "x-oss-meta-b" => "b"
+             }) == "x-oss-meta-a:a\nx-oss-meta-b:b\nx-oss-meta-c:c"
+
+      assert ApiRequest.canonicalize_oss_headers(%{
+               "x-oss-meta-a" => nil,
+               "x-oss-meta-c" => "c"
+             }) == "x-oss-meta-a\nx-oss-meta-c:c"
+    end
+
+    test "canonicalize_resource/3" do
+      assert ApiRequest.canonicalize_resource("example-bucket", "example-object", %{
+               uploadId: 2,
+               acl: nil
+             }) == "/example-bucket/example-object?acl&uploadId=2"
+
+      assert ApiRequest.canonicalize_resource("example-bucket", nil, %{
+               uploadId: 2,
+               acl: nil
+             }) == "/example-bucket/?acl&uploadId=2"
+
+      assert ApiRequest.canonicalize_resource(nil, nil, %{
+               uploadId: 2,
+               acl: nil
+             }) == "/?acl&uploadId=2"
+
+      assert ApiRequest.canonicalize_resource("example-bucket", "example-object", %{}) ==
+               "/example-bucket/example-object"
+    end
+
+    test "md5_hash/1" do
+      assert ApiRequest.md5_hash("0123456789") == "eB5eJF1ptWaXm4bijSPyxw=="
+    end
+  end
+end
