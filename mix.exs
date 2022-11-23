5c54a8874692c3b36bdeaeb235f007778e6b8a9b
c4710n <c4710n@users.noreply.github.com>
2022-11-23 20:34:12 +0800

    chore: prepare publishing

--- mix.exs
+++ mix.exs
@@ -1,13 +1,24 @@
 defmodule CozyOSS.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "SDK builder for RESTful API of Aliyun OSS / Alibaba Cloud OSS."
+  @source_url "https://github.com/cozy-elixir/cozy_oss"
+
   def project do
     [
       app: :cozy_oss,
+      version: @version,
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: @description,
+      source_url: @source_url,
+      homepage_url: @source_url,
+      docs: docs(),
+      package: package(),
+      aliases: aliases()
     ]
   end
 
@@ -24,7 +35,35 @@ defmodule CozyOSS.MixProject do
   defp deps do
     [
       {:sax_map, "~> 1.0"},
+      {:ex_doc, "~> 0.25", only: :dev, runtime: false},
       {:finch, "~> 0.13", only: [:dev, :test]}
     ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: @version
+    ]
+  end
+
+  defp package do
+    [
+      exclude_patterns: [],
+      licenses: ["Apache-2.0"],
+      links: %{GitHub: @source_url}
+    ]
+  end
+
+  defp aliases do
+    [publish: ["hex.publish", "tag"], tag: &tag_release/1]
+  end
+
+  defp tag_release(_) do
+    Mix.shell().info("Tagging release as #{@version}")
+    System.cmd("git", ["tag", @version])
+    System.cmd("git", ["push", "--tags"])
   end
 end
