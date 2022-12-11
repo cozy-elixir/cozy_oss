02d8ee2c730d4b4d986d9b0cadccbd1b00f8de7d
c4710n <c4710n@users.noreply.github.com>
2022-12-11 22:17:31 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CozyOSS.MixProject do
   use Mix.Project
 
-  @version "0.1.4"
+  @version "0.1.5"
   @description "An SDK builder for RESTful API of Aliyun OSS / Alibaba Cloud OSS."
   @source_url "https://github.com/cozy-elixir/cozy_oss"
 
